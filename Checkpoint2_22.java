b7a91547bc3487d79908114a8039758ca3057bf3
shahxvi <2025171523@student.uitm.edu.my>
2025-07-25 22:10:32 +0800

    Add files

---
+++ Checkpoint2_22.java
@@ -0,0 +1,12 @@
+public class Checkpoint2_22 {
+	public static void main(String[] args) {
+		System.out.println(6 + 3 * 5);
+		System.out.println(12 / 2 - 4);
+		System.out.println(9 + 14 * 2 - 6);
+		System.out.println(5 + 19 % 3 - 1);
+		System.out.println((6 + 2) * 3);
+		System.out.println(14 / (11-4));
+		System.out.println(9 + 12 * (8 - 3));
+	}
+}
+
---
+++ Checkpoint2_23.java
@@ -0,0 +1,7 @@
+public class Checkpoint2_23 {
+	public static void main(String[] args) {
+		double portion;
+		portion = 70 / 3;
+		System.out.println(portion);
+	}
+}
