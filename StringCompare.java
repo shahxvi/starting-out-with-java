5cae434252366a637e1d417c63296c7909d9afd1
shahxvi <2025171523@student.uitm.edu.my>
2025-07-28 19:01:17 +0800

    Practice String Comparison

---
+++ StringCompare.java
@@ -0,0 +1,11 @@
+public class StringCompare {
+	public static void main(String[] args) {
+		String name1 = "Shah";
+		String name2 = "Shah";
+
+		System.out.println(name1 == name2);
+		System.out.println(name1.equals(name2));
+		System.out.println(name1.compareTo(name2));
+	}
+}
+
