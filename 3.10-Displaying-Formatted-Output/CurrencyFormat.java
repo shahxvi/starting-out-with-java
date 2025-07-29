4520f492d395137baf7a71c2a6a67e5c9e540c2c
shahxvi <2025171523@student.uitm.edu.my>
2025-07-29 23:44:15 +0800

    Practice with prinf

---
+++ 3.10-Displaying-Formatted-Output/CurrencyFormat.java
@@ -0,0 +1,13 @@
+/**
+    This program demonstrates how to use the System.out.printf
+    method to format a number as currency
+*/
+
+public class CurrencyFormat {
+
+    public static void main(String[] args) {
+        double monthlyPay = 5000.0;
+        double annualPay = monthlyPay * 12;
+        System.out.printf("Your annual pay is RM%,.2f\n", annualPay);
+    }
+}
