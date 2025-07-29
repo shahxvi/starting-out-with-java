9a4ceb04a870d8a6db8494c371f07c9816395b0b
shahxvi <2025171523@student.uitm.edu.my>
2025-07-29 13:42:25 +0800

    Update files

--- 3.7-Conditional-Operator/ConditionalOperator.java
+++ 3.7-Conditional-Operator/ConditionalOperator.java
@@ -1,20 +1,16 @@
 public class ConditionalOperator {
 
     public static void main(String[] args) {
+        // a
         int x, y, z;
 
         x = 5;
         y = 6;
 
-        /*
-        if (x > y)
-             z = 1;
-        else
-             z = 20;
-        */
         z = x < y ? 1 : 20;
         System.out.println(z);
 
+        // b
         float temp = 40;
         int population;
         int base = 3;
@@ -22,5 +18,17 @@ public class ConditionalOperator {
         population = temp > 45 ? base * 10 : base * 2;
 
         System.out.println("Population = " + population);
+
+        // c
+        int hours = 18;
+        double wages = 0;
+        wages *= hours > 40 ? 1.5 : 1;
+        System.out.println("Wages = " + wages);
+
+        int result = 0;
+
+        System.out.println(
+            result >= 0 ? "The result is positive" : "The result is negative"
+        );
     }
 }
