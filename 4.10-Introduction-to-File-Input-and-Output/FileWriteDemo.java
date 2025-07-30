e14994a3fa2acc94903b8d97a92cc2219b95e234
shahxvi <2025171523@student.uitm.edu.my>
2025-07-30 09:08:12 +0800

    Pratice with File IO

---
+++ 4.10-Introduction-to-File-Input-and-Output/FileWriteDemo.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class FileWriteDemo {
+
+    public static void main(String[] args) throws IOException {
+        String fileName;
+        String friendName;
+        int numFriends;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("How many friends do you have? ");
+        numFriends = keyboard.nextInt();
+        keyboard.nextLine();
+
+        System.out.print("Enter the filename: ");
+        fileName = keyboard.nextLine();
+
+        PrintWriter outputFile = new PrintWriter(fileName);
+
+        for (int i = 0; i < numFriends; i++) {
+            System.out.print("Enter the name of friend #" + (i + 1) + " : ");
+            friendName = keyboard.nextLine();
+            outputFile.println(friendName);
+        }
+
+        keyboard.close();
+        outputFile.close();
+        System.out.println("Data written to " + fileName);
+    }
+}
---
+++ 4.10-Introduction-to-File-Input-and-Output/Notes.md
@@ -0,0 +1,11 @@
+Data may be saved in a file.
+There are three steps that are taken when a file is used by a program:
+1. The file must be *opened*. When the file is opened, a connection is created between the file and the program
+2. Data is then written to the file or read from the file
+3. When the program is finished using the file, the file must be *closed*.
+
+There's two types of files:
+1. Text
+2. Binary
+
+JavaAPI: `import java.io.*;`
