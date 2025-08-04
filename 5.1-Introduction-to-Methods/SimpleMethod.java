57a00960025ed6cf967617cc3af5c4c20b27caaa
shahxvi <2025171523@student.uitm.edu.my>
2025-08-04 22:53:52 +0800

    Practice with methods

---
+++ 5.1-Introduction-to-Methods/SimpleMethod.java
@@ -0,0 +1,13 @@
+public class SimpleMethod {
+
+	public static void main(String[] args) {
+		System.out.println("Hello form the main method.");
+		displayMessage();
+		System.out.println("Back in the main method.");
+	}
+
+	public static void displayMessage() {
+		System.out.println("Hello form the displayMessage method.");
+	}
+}	
+
