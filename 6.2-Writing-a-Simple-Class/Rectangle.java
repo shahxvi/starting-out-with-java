06b0ead240b07a1b09698b49b90e1cb739f7ae7e
shahxvi <2025171523@student.uitm.edu.my>
2025-08-09 23:54:00 +0800

    Practice writing a simple class

---
+++ 6.2-Writing-a-Simple-Class/Rectangle.java
@@ -0,0 +1,25 @@
+public class Rectangle {
+	private double length;
+	private double width;
+
+	public void setLength(double len) {
+		length = len;
+	}
+
+	public void setWidth(double w) {
+		width = w;
+	}
+
+	public double getLength() {
+		return length;
+	}
+
+	public double getWidth() {
+		return width;
+	}
+
+	public double getArea() {
+		return length * width;
+	}
+}
+
