6c2df2a9a52afb95324a68a6e0d0818c2aad195c
shahxvi <2025171523@student.uitm.edu.my>
2025-08-16 03:55:09 +0800

    Practice creating contructors and classes

---
+++ 6.4-Constructors/Cellphone.java
@@ -0,0 +1,37 @@
+public class Cellphone {
+
+	private String manufact;
+	private	String model;
+	private	double retailPrice;
+
+	public Cellphone(String man, String mod, double price) {
+		manufact = man;
+		model = mod;
+		retailPrice = price;
+	}
+
+	public void setManufact(String man) {
+		manufact = man;
+	}
+
+	public void setModel(String mod) {
+		model = mod;
+	}
+
+	public void setRetailPrice(String price) {
+		retailPrice = price;
+	}
+
+	public String getManufact() {
+		return manufact;
+	}
+
+	public String getModel() {
+		return model();
+	}
+
+	public double getRetailPrice() {
+		return retailPrice;
+	}
+}
+
