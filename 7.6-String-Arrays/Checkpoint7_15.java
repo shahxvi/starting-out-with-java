372645b30de43a0eb3257ccd9716e7821ffbb311
shahxvi <2025171523@student.uitm.edu.my>
2025-08-18 15:16:00 +0800

    Practice with String Arrays

---
+++ 7.6-String-Arrays/Checkpoint7_15.java
@@ -0,0 +1,15 @@
+public class Checkpoint7_15 {
+
+	public static void main(String[] args) {
+		String[] planets = {"Mercury", "Venus", "Earth", "Mars"};
+		
+		for (String str : planets) {
+			System.out.println(str);
+		}
+
+		for (String str : planets) {
+			System.out.println(str.charAt(0));
+		}
+	}
+}
+
