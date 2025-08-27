5867f55bda5bebb1c4f4e6f1cccb7d0cb8c01d33
shahxvi <2025171523@student.uitm.edu.my>
2025-08-28 01:12:21 +0800

    Ragged Arrays

---
+++ 7.09-Two-Dimensional-Arrays/RaggedArray.java
@@ -0,0 +1,14 @@
+public class RaggedArray {
+  public static void main(String[] args) {
+    int[][] ragged = new int[4][];
+
+    ragged[0] = new int[3];
+    ragged[1] = new int[4];
+    ragged[2] = new int[5];
+    ragged[3] = new int[6];
+
+    for (int index = 0; index < ragged.length; index++) {
+      System.out.println("The number of columes in row " + index + " is " + ragged[index].length);
+    }
+  }
+}
