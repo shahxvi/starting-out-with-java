c99fadda7b9e8fb5637f96419e0c3e5474a4516b
shahxvi <2025171523@student.uitm.edu.my>
2025-09-12 16:50:51 +0800

    Create a program that demonstrates passing objects as arguments

---
+++ 8.02-Passing-Objects-as-Arguments-to-Methods/PassObject.java
@@ -0,0 +1,18 @@
+public class PassObject {
+	public static void main(String[] args) {
+		Rectangle box = new Rectangle(12.0, 5.0);
+
+		displayRectangle(box);
+	}
+
+	/**
+	   The displayRectangle method displays the length and width of a rectangle
+	   @param r A reference to a Rectangle object
+	*/
+
+	public static void displayRectangle(Rectangle r) {
+		System.out.println("Length: " + r.getLength());
+		System.out.println("width: " + r.getWidth());
+	}
+}
+
